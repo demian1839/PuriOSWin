b93ceaf909888cceaa6b8f279e156fb145eef708
demian1839 <dggdemian@gmail.com>
2025-09-20 14:13:13 +0200

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -10,7 +10,7 @@ root.render(
   <Suspense
     fallback={
       <div id="sus-fallback">
-        <h1>Loading</h1>
+        <h1>Die Flimmerkiste Fährt hoch warte nh sekunde</h1>
       </div>
     }
   >
